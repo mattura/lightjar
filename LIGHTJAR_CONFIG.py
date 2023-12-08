349244dbf0b0203c96038334dbaa0ab9d22967c7
mattura <3749902+mattura@users.noreply.github.com>
2023-12-08 23:12:14 +0000

    Add files via upload

---
+++ LIGHTJAR_CONFIG.py
@@ -0,0 +1,9 @@
+#Plasma Stick config:
+NUM_LEDS = 50
+
+#MQTT config:
+#Please put your broker IP address or domain name here:
+BROKER = '192.168.1.XXX'
+TOPIC = 'LIGHTJAR'
+CLIENTID = 'JAR_1'
+PORT = 1883
